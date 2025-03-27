9264ec9b357dcc9920e7ece167cd4cc2c01ff342
Sakura <30777802+xjjxia@users.noreply.github.com>
2025-03-27 09:23:53 +0800

    Update main.js

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ import {GLTFLoader} from 'three/addons/loaders/GLTFLoader.js';
 // 创建场景、相机、渲染器
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-const renderer = new THREE.WebGLRenderer({ antialias: true });
+const renderer = new THREE.WebGLRenderer( { antialias: true, alpha: true } );
 renderer.setSize(window.innerWidth, window.innerHeight);
 renderer.setPixelRatio(window.devicePixelRatio);
 document.body.appendChild(renderer.domElement);
